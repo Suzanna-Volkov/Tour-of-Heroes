4dabe5c5915475b1fbf50abe9e557aeead5985fd
Zanna <svo@it-minds.dk>
2020-01-17 11:31:54 +0100

    Added routing to the tutorial and their styling code

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,14 +3,24 @@ import { Hero } from '../models/hero';
 import { HEROES } from '../mock-heroes';
 import { Observable, of} from 'rxjs';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: Send the message _after_ fetching the heroes.
+    this.messageService.add('HeroService: Heroes have been fetched!');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: Hero id=${id} has been fetched!`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
